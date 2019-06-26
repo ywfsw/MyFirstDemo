9207498e11894e1b51e12b305b43ebc9370dcf0a
gakkiiiii <ywfswywfsw@gmail.com>
2019-06-26 14:40:02 +0800

    UPdate

--- MyFirstDemo/ViewController.swift
+++ MyFirstDemo/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        let QAQ = UILabel(frame: CGRect(x: 30,y: 80,width: 310,height: 60))
+        QAQ.textColor = UIColor.red
+        QAQ.font = UIFont.systemFont(ofSize: 15)
+        QAQ.text = "QAQ"
+        self.view.addSubview(QAQ)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
